29672d7dc8724cf6d47bc790d06cc0405c7e8903
Arun Kumar <arun19091998@gmail.com>
2018-05-24 11:13:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+const compression = require('compression');
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(compression());
 
 app.get('/ping', function (req, res) {
  return res.send('pong');
