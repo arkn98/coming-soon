d277b98249f75357b30034f37fd6adbe61700c3a
Arun Kumar <arun19091998@gmail.com>
2018-05-24 10:39:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
