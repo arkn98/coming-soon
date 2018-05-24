1274ab4d3314e1febd14e55d49975f94b35e2c1e
Arun Kumar <arun19091998@gmail.com>
2018-05-24 09:19:44 +0530

    Express server configure

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require('express');
-
+const bodyParser = require('body-parser')
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, 'build')));
 
-/* app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-}); */
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-app.use( express.static( `${__dirname}/../build` ) );
-
-const path = require('path')
-app.get('*', (req, res)=>{
-  res.sendFile(path.join(__dirname, '../build/index.html'));
-})
+app.listen(process.env.PORT || 8080);
