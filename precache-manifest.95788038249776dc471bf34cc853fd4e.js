2911d33f1a2d0225af7a93dc4917ac6a592581c2
arkn98 <20590666+arkn98@users.noreply.github.com>
2019-10-27 17:37:05 +0530

    Updates

---
+++ precache-manifest.95788038249776dc471bf34cc853fd4e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e1aa34c66f9a118ef30450f9921ef65",
+    "url": "/coming-soon/index.html"
+  },
+  {
+    "revision": "5a7d8cc3d50ecfc526d2",
+    "url": "/coming-soon/static/css/main.66b02c06.chunk.css"
+  },
+  {
+    "revision": "c7867873e5dca6609f9b",
+    "url": "/coming-soon/static/js/2.cd359f5c.chunk.js"
+  },
+  {
+    "revision": "5a7d8cc3d50ecfc526d2",
+    "url": "/coming-soon/static/js/main.296cd08c.chunk.js"
+  },
+  {
+    "revision": "637218e107729168d904",
+    "url": "/coming-soon/static/js/runtime-main.d1f8a7b2.js"
+  },
+  {
+    "revision": "5159a5d89abe8bf68b09b569dbeccbc0",
+    "url": "/coming-soon/static/media/IBMPlexSans-Bold.5159a5d8.ttf"
+  },
+  {
+    "revision": "ee425cc83f37323665790c89758cf359",
+    "url": "/coming-soon/static/media/IBMPlexSans-BoldItalic.ee425cc8.ttf"
+  },
+  {
+    "revision": "40bbef08ca6f6edea2a9a9e882541ce0",
+    "url": "/coming-soon/static/media/IBMPlexSans-Italic.40bbef08.ttf"
+  },
+  {
+    "revision": "29047654270fd882ab9e9ec10e28f7c5",
+    "url": "/coming-soon/static/media/IBMPlexSans-Light.29047654.ttf"
+  },
+  {
+    "revision": "453b2bbf7ad0bb52a93f64ac96641f24",
+    "url": "/coming-soon/static/media/IBMPlexSans-LightItalic.453b2bbf.ttf"
+  },
+  {
+    "revision": "ee83103a4a777209b0f759a4ff598066",
+    "url": "/coming-soon/static/media/IBMPlexSans-Medium.ee83103a.ttf"
+  },
+  {
+    "revision": "eb7dadea8e7c37ce1a1406045dda7c1e",
+    "url": "/coming-soon/static/media/IBMPlexSans-MediumItalic.eb7dadea.ttf"
+  },
+  {
+    "revision": "c02b4dc6554c116e4c40f254889d5871",
+    "url": "/coming-soon/static/media/IBMPlexSans-Regular.c02b4dc6.ttf"
+  },
+  {
+    "revision": "50f13391babe5b650b8a0589dd962f74",
+    "url": "/coming-soon/static/media/facebook.50f13391.svg"
+  },
+  {
+    "revision": "baf48a32fd45986dfb5b52aada1b558a",
+    "url": "/coming-soon/static/media/github.baf48a32.svg"
+  },
+  {
+    "revision": "a50716dd845ea43407a9a1bcffe42aef",
+    "url": "/coming-soon/static/media/twitter.a50716dd.svg"
+  }
+]);
